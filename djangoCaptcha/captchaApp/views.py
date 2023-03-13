284daf9c4cecbe91741624538c8d44828c439f96
arian <arian_gh22@yahoo.com>
2023-03-13 12:16:24 +0330

    defining suitable method in views.py

--- djangoCaptcha/captchaApp/views.py
+++ djangoCaptcha/captchaApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import HumanOnlyDataSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def securityChecker(request):
+   data = request.data
+   serializer = HumanOnlyDataSerializer(data=data)
+   if serializer.is_valid():
+      return Response("verified")
+   else:
+      return Response("not verfied")
